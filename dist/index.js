a443078f656fc3d0ea86bea8d5945dcd0cbe40de
Vincent <git@vincenttunru.com>
2025-03-06 16:08:25 +0100

    Update build artefacts
    Getting tired of this already

--- dist/index.js
+++ dist/index.js
@@ -36639,10 +36639,10 @@ const githubTokenPermissionsCheck = async (configs) => {
 function getWarningsForWorkflow$1(workflowPath, workflowConfig) {
     const warnings = [];
     if (!workflowConfig.permissions) {
-        warnings.push(`Job [${workflowPath}] has not set the least required privileges for $GITHUB_TOKEN. See https://docs.github.com/en/actions/writing-workflows/workflow-syntax-for-github-actions#defining-access-for-the-github_token-scopes.`);
+        warnings.push(`Workflow [${workflowPath}] has not set the least required privileges for $GITHUB_TOKEN. See https://docs.github.com/en/actions/writing-workflows/workflow-syntax-for-github-actions#defining-access-for-the-github_token-scopes.`);
     }
     else if (Object.values(workflowConfig.permissions).length !== 0) {
-        warnings.push(`Job [${workflowPath}] has defined non-empty workflow-level permissions for $GITHUB_TOKEN. To ensure least privileges, set the minimal permissions at the job, rather than workflow, level. See https://docs.github.com/en/actions/writing-workflows/workflow-syntax-for-github-actions#jobsjob_idpermissions.`);
+        warnings.push(`Workflow [${workflowPath}] has defined non-empty workflow-level permissions for $GITHUB_TOKEN. To ensure least privileges, set the minimal permissions at the job, rather than workflow, level. See https://docs.github.com/en/actions/writing-workflows/workflow-syntax-for-github-actions#jobsjob_idpermissions.`);
     }
     return warnings;
 }
